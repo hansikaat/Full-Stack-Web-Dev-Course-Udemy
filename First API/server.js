03fca3068969ca3f27204018f584dc867668b8f8
Hansika Attanayake <hansika.attanayake@ericsson.com>
2019-07-22 13:11:56 +0200

    GET, POST and Postman

--- First API/server.js
+++ First API/server.js
@@ -1,8 +1,41 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+var ingredients = [
+  {
+    "id": "232kAk",
+    "text": "Eggs"
+  },
+  {
+    "id": "dkP345",
+    "text": "Milk"
+  },
+  {
+    "id": "dkcuu7",
+    "text": "Bacon"
+  },
+  {
+    "id": "73hdy",
+    "text": "Chicken Legs"
+  }
+];
 
 app.get('/',function(req, res) {
-  res.send('My First API!');
+  res.send(ingredients);
+});
+
+app.post('/', function(req, res) {
+  var ingredient = req.body;
+    if(!ingredient || ingredient.text == "") {
+      res.status(500).send({error: "Your ingredient must have text"});
+    } else {
+      ingredients.push(ingredient);
+      res.status(200).send(ingredients);
+    }
 });
 
 app.get('/funions',function(req, res) {
