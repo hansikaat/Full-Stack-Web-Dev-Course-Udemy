e8ab226dcedcea2553ee480fee317fe98aec01d8
Hansika Attanayake <hansika.attanayake@ericsson.com>
2019-07-24 14:25:09 +0200

    Components in React

--- swag-shop-web/src/app/App.js
+++ swag-shop-web/src/app/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Product from '../product/product';
 import HttpService from '../services/http-service';
 
 const http = new HttpService();
@@ -24,20 +25,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h2>Welcome to React.</h2>
+        </div>
+        <div className="App-main">
+          <Product />
+        </div>
       </div>
     );
   }
