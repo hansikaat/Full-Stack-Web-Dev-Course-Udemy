a8e8f8b7e796deb15a6473e15011b84b19397308
Hansika Attanayake <hansika.attanayake@ericsson.com>
2019-07-24 15:01:30 +0200

    working with state in React

--- swag-shop-web/src/app/App.js
+++ swag-shop-web/src/app/App.js
@@ -10,16 +10,30 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {products:[]};
+
     this.loadData =this.loadData.bind(this);
+    this.productList = this.productList.bind(this);
     this.loadData();
   }
 
   loadData = () => {
-    http.getProducts().then(products => {
-      console.log(products);
+    var self = this;
+    http.getProducts().then(data => {
+      self.setState({products: data})
     }, err => {
 
     });
+  }
+
+  productList = () => {
+    const list = this.state.products.map((product) => 
+      <div className="col-sm-4" key={product._id}>
+        <Product title={product.title} price={product.price} imgUrl={product.imgUrl}/>
+      </div>
+    );
+    return (list);
   }
 
   render() {
@@ -31,9 +45,7 @@ class App extends Component {
         </div>
         <div className="App-main">
           <div className="row">
-            <Product className="col-sm-4" price="4.23" title="Cool Toy Gun" imgUrl="https://image.freepik.com/free-photo/swimming-pool-water-with-copy-space_23-2148240100.jpg"/>
-            <Product className="col-sm-4" price="4.23" title="Cool Toy Gun" imgUrl="https://image.freepik.com/free-photo/swimming-pool-water-with-copy-space_23-2148240100.jpg"/>
-            <Product className="col-sm-4" price="4.23" title="Cool Toy Gun" imgUrl="https://image.freepik.com/free-photo/swimming-pool-water-with-copy-space_23-2148240100.jpg"/>
+            {this.productList()}
           </div>
         </div>
       </div>
